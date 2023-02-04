fd8765b33fd62e07171d9174799eac1d58dad007
Lachlan Holland <lachholland@gmail.com>
2023-02-04 17:16:30 +1000

    clean

--- main.py
+++ main.py
@@ -1,47 +1,14 @@
-import torch
-import torch.nn as nn
-from model.Recommender import Recommender
-from model.Tower import Tower
-import datasets.ItemDataset as ItemDataset
-import datasets.UserDataset as UserDataset
-
-
-def custom_cross_entropy_loss(logits, true_labels, training):
-    batch_size, nb_candidates = logits.shape
-    if training:
-        label_probs =
-        logits -= torch.log(label_probs)
-        true_labels = tf.range(0, batch_size)
-    loss = nn.CrossEntropyLoss(logits, true_labels)
-    return {'loss': loss, 'logits': logits}
-
-
-def train_step(model, user_train, item_train, epoch):
-    logits = model(user_train, item_train)
-    criterion = nn.CrossEntropyLoss()
-    loss_value = criterion(logits, item_train)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    optimizer.zero_grad()
-    loss_value.backward()
-    optimizer.step()
-    return {'loss': loss_value, 'epoch': epoch}
-
-
-def train_model(model):
-    item_train = ItemDataset(csv_file='data/item_train.csv')
-    user_train = UserDataset(csv_file='data/user_train.csv')
-    loss_history = []
-    for epoch in range(10):
-        result = train_step(model, user_train, item_train, epoch)
-        loss_history.append(result)
-
+from data.util import util
+from model.util import util
+from model import  RecommenderSystem
+from model import RecommenderTowerModel
 
 def main():
-    user_model = Tower()
-    item_model = Tower()
-    tower_model = Recommender(user_model, item_model)
-    train_model(tower_model)
-
+    util.data_init()
+    user_model = RecommenderTowerModel()
+    item_model = RecommenderTowerModel()
+    tower_model = RecommenderSystem(user_model, item_model)
+    model.util.train_model(tower_model, epochs=10)
 
 if __name__ == '__main__':
     main()
--- model/RecommenderTowerModel.py
+++ model/RecommenderTowerModel.py
@@ -1,14 +1,14 @@
 import torch.nn as nn
 
-class Tower(nn.Module):
+class RecommenderTowerModel(nn.Module):
     def __init__(self, vocab_size: int, embedding_dimension: int):
-        super(Tower, self).__init__()
+        super().__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dimension)
         self.fc = nn.Linear(256, 120)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(embedding_dimension)
         self.relu2 = nn.ReLU()
-
+        
     def forward(self, inputs):
         x = self.embedding(inputs)
         x = self.fc(x)
@@ -16,4 +16,3 @@ class Tower(nn.Module):
         x = self.fc2(x)
         x = self.relu2(x)
         return x
-    
