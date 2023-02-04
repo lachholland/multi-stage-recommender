2fe729df626baadbdd8792f66a657a5fa2482dfc
Lachlan Holland <lachholland@gmail.com>
2023-02-04 17:28:43 +1000

    dads

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from data.util import util
+from data import util as data_util
 from model import util as model_util
 from model import RecommenderSystem
 from model import RecommenderTowerModel
 
 def main():
-    util.data_init()
+    data_util.data_init()
     user_model = RecommenderTowerModel()
     item_model = RecommenderTowerModel()
     tower_model = RecommenderSystem(user_model, item_model)
-    model.util.train_model(tower_model, epochs=10)
+    data_util.train_model(tower_model, epochs=10)
 
 if __name__ == '__main__':
     main()
