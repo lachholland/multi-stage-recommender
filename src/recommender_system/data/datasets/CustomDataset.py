4362c37027acecba8bff082e3933a6410efe2442
Lachlan Holland <lachholland@gmail.com>
2023-02-06 21:57:57 +1000

    fixed package

--- src/recommender_system/data/datasets/CustomDataset.py
+++ src/recommender_system/data/datasets/CustomDataset.py
@@ -1,19 +1,18 @@
-import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 import pandas as pd
 
 class CustomDataset(Dataset):
-    def __init__(self, transactions_train_data:pd.DataFrame, transform=None,target_transform=None):
-        self.transactions_train_data = transactions_train_data
+    def __init__(self, transactions__data:pd.DataFrame, transform=None,target_transform=None):
+        self.transactions_data = transactions__data
         self.transform=transform  # x value transform
         self.target_transform=target_transform  #y value transform
 
     def __len__(self):
-        return len(self.transactions_train_data)
+        return len(self.transactions_data)
 
     def __getitem__(self, index):
-        user=self.transactions_train_data['customer_id'][index] #fetches the x value (user)
-        label=self.transactions_train_data['article_id'][index] #fetches they y value (article)
+        user=self.transactions_data['customer_id'][index] #fetches the x value (user)
+        label=self.transactions_data['article_id'][index] #fetches they y value (article)
         if self.transform:
             user=self.transform(user)  #transforms the x value if transformation exists
         if self.target_transform:
