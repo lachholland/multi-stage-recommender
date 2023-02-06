e3d1e1c945e4d4b34eab2f4efbc73efb89db2633
navpat <nav.pathak18@gmail.com>
2023-02-06 19:43:12 +0530

    added DataLoaderCreator function and CustomDatasetCreator function

---
+++ data/datasets/CustomDataset.py
@@ -0,0 +1,22 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+
+class CustomDataset(Dataset):
+    def __init__(self, transactions_train_data:pd.DataFrame, transform=None,target_transform=None):
+        self.transactions_train_data = transactions_train_data
+        self.transform=transform  # x value transform
+        self.target_transform=target_transform  #y value transform
+
+    def __len__(self):
+        return len(self.transactions_train_data)
+
+    def __getitem__(self, index):
+        user=self.transactions_train_data['customer_id'][index] #fetches the x value (user)
+        label=self.transactions_train_data['article_id'][index] #fetches they y value (article)
+        if self.transform:
+            user=self.transform(user)  #transforms the x value if transformation exists
+        if self.target_transform:
+            label=self.target_transform(label)  #transforms the y value if target_transformation exists
+        return user,label
+ 
--- src/recommender_system/data/util/data_util.py
+++ src/recommender_system/data/util/data_util.py
@@ -3,6 +3,8 @@ from torchtext.vocab import build_vocab_from_iterator
 import pandas as pd
 import os
 from recommender_system.data.datasets import CustomDataset
+import numpy as np
+from torch.utils.data.sampler import SubsetRandomSampler
 
 def split_data(train_decimal=0.6, val_decimal=0.2): 
     data = pd.read_csv(os.path.join(os.getcwd(), 'data', 'data_files', 'transactions.csv')) 
@@ -12,14 +14,15 @@ def split_data(train_decimal=0.6, val_decimal=0.2):
     train_data, val_data, test_data = torch.utils.data.random_split(data, [train_size, val_size, test_size])
     return train_data, val_data, test_data
 
-def dataset_init(transactions_train_data):
+def CustomDatasetCreator(transactions_train_data):
     transactions_train_data = pd.read_csv('data/transactions_train.csv')
     pd.read_csv('data/transactions_train.csv')
     transform=lambda x:customer_lookup(transactions_train_data).__getitem__(x)
     target_transform=lambda y:article_lookup(transactions_train_data).__getitem__(y)
     dataset = CustomDataset(transactions_train_data,transform=transform,target_transform=target_transform)
-    dataloader = torch.utils.data.Dataloader(dataset, batch_size=2, shuffle=True)
-    return dataloader   
+    return dataset
+
+
 
 def article_lookup(train_df:pd.DataFrame):
     unique_article_ids=train_df.article_id.unique() # list of unique article_ids found in training dataset
@@ -38,3 +41,20 @@ def customer_lookup(train_df:pd.DataFrame):
 def yield_tokens(unique_ids):
     for id in unique_ids:
         yield id
+
+def DataLoaderCreator(dataset,batch_size,splits,shuffle_dataset=True,random_seed=42):
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+    split = int(np.floor(splits * dataset_size))
+    if shuffle_dataset :
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+    train_indices = indices[split:]
+    val_indices=indices[:split]
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, 
+                                           sampler=train_sampler)
+    validation_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                                sampler=valid_sampler)
+    return train_loader,validation_loader
