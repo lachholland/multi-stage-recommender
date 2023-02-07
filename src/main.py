5702f581b2f5b9088a2f7e0e0a789d9920f7a3b1
navpat <nav.pathak18@gmail.com>
2023-02-07 16:22:58 +0530

    all imports working and main.py modified

--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
-from recommender_system.data.util import data_util
-from recommender_system.model.util import train_util
-from recommender_system.model import RecommenderSystem
-from recommender_system.model import RecommenderTowerModel
+from .recommender_system.data.util import data_util
+from .recommender_system.model.util import train_util
+from .recommender_system.model import RecommenderSystem
+from .recommender_system.model import RecommenderTowerModel
 from torch.utils.data import DataLoader
-from recommender_system.data.datasets import CustomDataset
+from .recommender_system.data.datasets import CustomDataset
+import pandas as pd
 
 def main():
+    transactions_train_data=pd.read_csv(r'C:\Users\navpa\recommender_system\multi_stage_recommender.git\data\transactions_train.csv')
     item_model = RecommenderTowerModel()
     user_model = RecommenderTowerModel()
     recommender_system = RecommenderSystem(user_model, item_model)
-    train_data, test_data, val_data = data_util.split_data()
-    train_data_loader = data_util.dataset_init(train_data) 
+    complete_customdataset = data_util.CustomDatasetCreator(transactions_train_data)
+    train_loader,validation_loader=data_util.DataLoaderCreator(complete_customdataset,batch_size=64,splits=0.2)
     train_util.train_recommender_system(recommender_system, train_data_loader, epochs=10)
 
 if __name__ == '__main__':
