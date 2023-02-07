1e4f1fcdcff25061af8e5183be764a56eb92cfc0
navpat <nav.pathak18@gmail.com>
2023-02-07 18:19:56 +0530

    made src a package

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
-from .recommender_system.data.util import data_util
-from .recommender_system.model.util import train_util
-from .recommender_system.model import RecommenderSystem
-from .recommender_system.model import RecommenderTowerModel
+from recommender_system.data.util import data_util
+from recommender_system.model.util import train_util
+from recommender_system.model import RecommenderSystem
+from recommender_system.model import RecommenderTowerModel
 from torch.utils.data import DataLoader
-from .recommender_system.data.datasets import CustomDataset
+from recommender_system.data.datasets import CustomDataset
 import pandas as pd
 
 def main():
     transactions_train_data=pd.read_csv(r'.\recommender_system\data\transactions_train.csv')
-    item_model = RecommenderTowerModel()
-    user_model = RecommenderTowerModel()
-    recommender_system = RecommenderSystem(user_model, item_model)
+    item_model = RecommenderTowerModel.RecommenderTowerModel()
+    user_model = RecommenderTowerModel.RecommenderTowerModel()
+    recommender_system = RecommenderSystem.RecommenderSystem(user_model, item_model)
     complete_customdataset = data_util.CustomDatasetCreator(transactions_train_data)
     train_data_loader,validation_loader=data_util.DataLoaderCreator(complete_customdataset,batch_size=64,splits=0.2)
     train_util.train_recommender_system(recommender_system, train_data_loader, epochs=10)
