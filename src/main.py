d0eb95d435aed0c7eb24dd90f77fef8b11297a07
navpat <nav.pathak18@gmail.com>
2023-02-07 16:45:18 +0530

    added relative path in main.py and variable name changes

--- src/main.py
+++ src/main.py
@@ -7,12 +7,12 @@ from .recommender_system.data.datasets import CustomDataset
 import pandas as pd
 
 def main():
-    transactions_train_data=pd.read_csv(r'C:\Users\navpa\recommender_system\multi_stage_recommender.git\data\transactions_train.csv')
+    transactions_train_data=pd.read_csv(r'.\recommender_system\data\transactions_train.csv')
     item_model = RecommenderTowerModel()
     user_model = RecommenderTowerModel()
     recommender_system = RecommenderSystem(user_model, item_model)
     complete_customdataset = data_util.CustomDatasetCreator(transactions_train_data)
-    train_loader,validation_loader=data_util.DataLoaderCreator(complete_customdataset,batch_size=64,splits=0.2)
+    train_data_loader,validation_loader=data_util.DataLoaderCreator(complete_customdataset,batch_size=64,splits=0.2)
     train_util.train_recommender_system(recommender_system, train_data_loader, epochs=10)
 
 if __name__ == '__main__':
